0ebef8bbc141da85a131fa5b7270b222de348d11
Matt Williams <matwilliams@hotmail.com>
2016-05-14 16:41:20 +0000

    Basic streaming

--- index.js
+++ index.js
@@ -26,91 +26,50 @@ function (err, data) {
 var fs = require("fs");
 var path = require("path");
 app.get('/video.mp4', function(req, res) {
-
-  var file = path.resolve(__dirname,"public/video.mp4");
-  fs.stat(file, function(err, stats) {
-    console.log(stats);
+  var key = "video.mp4";
+  s3.headObject({Key: key}, function(err, data) {
+    console.log("headObject:", err, data);
     if (err) {
-      if (err.code === 'ENOENT') {
-        // 404 Error if file not found
-        return res.sendStatus(404);
+      // TODO: Handle better!
+      res.end(err);
+    } else {
+      var total = data.ContentLength;
+  
+      var range = req.headers.range;
+      var start;
+      var end;
+      if (range) {
+        var positions = range.replace(/bytes=/, "").split("-");
+        var start = parseInt(positions[0], 10);
+        var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+      } else {
+        start = 0;
+        end = total - 1;
       }
-    res.end(err);
+      var chunksize = (end - start) + 1;
+    
+        res.writeHead(206, {
+          "Content-Range": "bytes " + start + "-" + end + "/" + total,
+          "Accept-Ranges": "bytes",
+          "Content-Length": chunksize,
+          "Content-Type": "video/mp4"
+        });
+    
+      s3.getObject({Key: key, Range: req.headers.range}).createReadStream().pipe(res);
     }
-    var range = req.headers.range;
-    if (!range) {
-     // 416 Wrong range
-     return res.sendStatus(416);
-    }
-    var positions = range.replace(/bytes=/, "").split("-");
-    var start = parseInt(positions[0], 10);
-    var total = stats.size;
-    var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
-    var chunksize = (end - start) + 1;
+  });
+});
 
-    res.writeHead(206, {
-      "Content-Range": "bytes " + start + "-" + end + "/" + total,
-      "Accept-Ranges": "bytes",
-      "Content-Length": chunksize,
-      "Content-Type": "video/mp4"
-    });
-
-    var stream = fs.createReadStream(file, { start: start, end: end })
-      .on("open", function() {
-        stream.pipe(res);
-      }).on("error", function(err) {
-        res.end(err);
-      });
+app.put('/video.mp4', function(req, res) {
+  var key = "video.mp4";
+  s3.upload({Key: key, Body: req}, function(err, data) {
+    console.log("upload", err, data);
+    res.end();
   });
 });
 
 
 
-/*
-    url = require("url"),
-
-http.createServer(function (req, res) {
-  if (req.url != "/movie.mp4") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end('<video src="http://localhost:8888/movie.mp4" controls></video>');
-  } else {
-    var file = path.resolve(__dirname,"movie.mp4");
-    fs.stat(file, function(err, stats) {
-      if (err) {
-        if (err.code === 'ENOENT') {
-          // 404 Error if file not found
-          return res.sendStatus(404);
-        }
-      res.end(err);
-      }
-      var range = req.headers.range;
-      if (!range) {
-       // 416 Wrong range
-       return res.sendStatus(416);
-      }
-      var positions = range.replace(/bytes=/, "").split("-");
-      var start = parseInt(positions[0], 10);
-      var total = stats.size;
-      var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
-      var chunksize = (end - start) + 1;
-
-      res.writeHead(206, {
-        "Content-Range": "bytes " + start + "-" + end + "/" + total,
-        "Accept-Ranges": "bytes",
-        "Content-Length": chunksize,
-        "Content-Type": "video/mp4"
-      });
-
-      var stream = fs.createReadStream(file, { start: start, end: end })
-        .on("open", function() {
-          stream.pipe(res);
-        }).on("error", function(err) {
-          res.end(err);
-        });
-    });
-  }
-}).listen(8888);
-*/
 
 app.use('/', express.static(__dirname + '/public'));
 
