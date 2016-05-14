435f704b31ab28d0c4253a96122d7c5a2be88657
Matt Williams <matwilliams@hotmail.com>
2016-05-14 15:54:56 +0000

    Basic AWS integration and (local) video streaming

--- index.js
+++ index.js
@@ -1,7 +1,118 @@
 var express = require('express');
+var aws = require('aws-sdk');
+var config = require('./config.json');
 
 var app = express();
 
+var s3 = new aws.S3({params: {Bucket: 'mastery-app'}});
+s3.listObjects({}, function(err, data) {
+  if (err) {
+    console.log("Error uploading data: ", err);
+  } else {
+    console.log("Successfully uploaded data to myBucket/myKey", data);
+  }
+});
+
+var dynamoUsers = new aws.DynamoDB({params: {TableName: 'mastery-users'}});
+dynamoUsers.getItem(
+{"Key" : { "id" : { "S" : "bob" } } },
+function (err, data) {
+  console.log("getItem", err, data);
+});
+
+
+
+
+var fs = require("fs");
+var path = require("path");
+app.get('/video.mp4', function(req, res) {
+
+  var file = path.resolve(__dirname,"public/video.mp4");
+  fs.stat(file, function(err, stats) {
+    console.log(stats);
+    if (err) {
+      if (err.code === 'ENOENT') {
+        // 404 Error if file not found
+        return res.sendStatus(404);
+      }
+    res.end(err);
+    }
+    var range = req.headers.range;
+    if (!range) {
+     // 416 Wrong range
+     return res.sendStatus(416);
+    }
+    var positions = range.replace(/bytes=/, "").split("-");
+    var start = parseInt(positions[0], 10);
+    var total = stats.size;
+    var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+    var chunksize = (end - start) + 1;
+
+    res.writeHead(206, {
+      "Content-Range": "bytes " + start + "-" + end + "/" + total,
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunksize,
+      "Content-Type": "video/mp4"
+    });
+
+    var stream = fs.createReadStream(file, { start: start, end: end })
+      .on("open", function() {
+        stream.pipe(res);
+      }).on("error", function(err) {
+        res.end(err);
+      });
+  });
+});
+
+
+
+/*
+    url = require("url"),
+
+http.createServer(function (req, res) {
+  if (req.url != "/movie.mp4") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end('<video src="http://localhost:8888/movie.mp4" controls></video>');
+  } else {
+    var file = path.resolve(__dirname,"movie.mp4");
+    fs.stat(file, function(err, stats) {
+      if (err) {
+        if (err.code === 'ENOENT') {
+          // 404 Error if file not found
+          return res.sendStatus(404);
+        }
+      res.end(err);
+      }
+      var range = req.headers.range;
+      if (!range) {
+       // 416 Wrong range
+       return res.sendStatus(416);
+      }
+      var positions = range.replace(/bytes=/, "").split("-");
+      var start = parseInt(positions[0], 10);
+      var total = stats.size;
+      var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+      var chunksize = (end - start) + 1;
+
+      res.writeHead(206, {
+        "Content-Range": "bytes " + start + "-" + end + "/" + total,
+        "Accept-Ranges": "bytes",
+        "Content-Length": chunksize,
+        "Content-Type": "video/mp4"
+      });
+
+      var stream = fs.createReadStream(file, { start: start, end: end })
+        .on("open", function() {
+          stream.pipe(res);
+        }).on("error", function(err) {
+          res.end(err);
+        });
+    });
+  }
+}).listen(8888);
+*/
+
 app.use('/', express.static(__dirname + '/public'));
+
 
 app.listen(process.env.PORT || 80);
